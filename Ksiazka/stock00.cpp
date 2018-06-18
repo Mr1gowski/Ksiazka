d1557059c992337eec4946b4211d452e10b9e462
Jakub Langowski <kusciana@gmail.com>
2018-06-18 06:21:47 +0200

    vec

--- Ksiazka/stock00.cpp
+++ Ksiazka/stock00.cpp
@@ -1,71 +1,27 @@
-#include <iostream>
+#include <cmath>
 #include "stock00.h"
 
+using std::sqrt;
+using std::sin;
+using std::cos;
+using std::atan;
+using std::atan2;
+using std::cout;
 
-Time::Time()
+namespace VECTOR
 {
-	hours = minutes = 0;
+	const double Rad_to_Reg = 45 / atan(1.0);
+
+	void Vector::setmag()
+	{
+		mag = sqrt(x*x + y * y);
+	}
+	void Vector::setang()
+	{
+
+	}
+
+
+
+
 }
-
-Time::Time(int h, int m)
-{
-	hours = h;
-	minutes = m;
-}
-
-void Time::admin(int m)
-{
-	minutes += m;
-	hours += minutes / 60;
-	minutes %= 60;
-}
-
-void Time::adhr(int h)
-{
-	hours += h;
-}
-
-void Time::reset(int h, int m)
-{
-	hours = h;
-	minutes = m;
-}
-
-Time Time::operator+(const Time & t) const
-{
-	Time sum;
-	sum.minutes = minutes + t.minutes;
-	sum.hours = hours + t.hours + sum.minutes / 60;
-	sum.minutes %= 60;
-	return sum;
-}
-Time Time::operator-(const Time &t)const
-{
-	Time tiff;
-	int tot1, tot2;
-	tot1 = t.minutes + 60 * t.hours;
-	tot2 = minutes + 60 * hours;
-	tiff.minutes=(tot1 - tot2) % 60;
-	tiff.hours = (tot1 - tot2) / 60;
-	return tiff;
-}
-
-Time Time::operator*(double mult) const
-{
-	Time result;
-	long totm = hours * mult * 60 + minutes * mult;
-	result.hours = totm / 60;
-	result.minutes = totm % 60;
-
-	return result;
-}
-
-
-
-
-ostream & operator<<(ostream & os, const Time &t)
-{
-	os << t.hours << " godzin, " << t.minutes << " minut.";
-	return os;
-}
-
--- Ksiazka/stock00.h
+++ Ksiazka/stock00.h
@@ -1,34 +1,44 @@
 #pragma once
-#include <string>
-
-#ifndef  MYTIEM_H_
-#define MYTIME_H_
-
+#ifndef  MYTIME_H_
+#define  MYTIME_H_
 #include <iostream>
-using std::ostream;
-
-class Time
+namespace vector
 {
-public:
-	Time();
-	Time(int h, int m = 0);
-	void admin(int m);
-	void adhr(int h);
-	void reset(int h = 0, int m = 0);
-	Time operator+(const Time &t)const;
-	Time operator-(const Time &t)const;
-	Time operator*(double m)const;
-	friend Time operator*(double m, const Time &t)
-	{	return t * m;  }
 
-	friend ostream & operator<<(ostream & os,const Time &t);
+	
+	class Vector
+	{
+	public:
+		enum Mode {RECT,POL};
+		Vector();
+		Vector(double n1, double n2, Mode form = RECT);
+		void reset(double n1, double n2, Mode form = RECT);
+		~Vector();
+		double xval()const { return x; }
+		double yval()const { return y; }
+		double magval()const { return mag; }
+		double angval()const { return ang; }
 
-private:
-	int hours;
-	int minutes;
+		void polar_mode();
+		void rect_mode();
 
-};
+		Vector operator+(const Vector & b)const;
+		Vector operator-(const Vector & b)const;
+		Vector operator-()const;
+		Vector operator*(double n)const;
+		friend Vector operator*(double n, const Vector & a);
+		friend std::ostream&
+			operator<<(std::ostream & os, const Vector & v);
 
+	private:
+		double x, y, mag, ang;
+		Mode mode;
+		void setx();
+		void sety();
+		void setmag();
+		void setang();
+	};
+}
 
 
 #endif //  MYTIME_H_
