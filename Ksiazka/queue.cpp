6c5278b62ac4ea4b61ebb4081d91fdc2b6ac92e9
Jakub Langowski <kusciana@gmail.com>
2018-07-08 05:44:39 +0200

    nie dziala

--- Ksiazka/queue.cpp
+++ Ksiazka/queue.cpp
@@ -1,124 +1,112 @@
 #include <iostream>
 #include "queue.h"
+
 using std::cout;
+using std::ios_base;
 using std::endl;
+
 using std::string;
-//formatowanie napisów
-typedef std::ios_base::fmtflags format;
-typedef std::streamsize precis;
-format setFormat();
-void restore(format f, precis p);
-//---------------------------------
 
-Brass::Brass(const string &s, long an, double bal)
+AcctABC::AcctABC(const string &s, long an, double bal)
 {
-	fullname = s;
-	accNum = an;
+	fullName = s;
+	acctNum = an;
 	balance = bal;
-
 }
 
-void Brass::Deposit(double amt)
+void AcctABC::Deposit(double amt)
 {
-	if (amt < 0)
-		cout << "Nie mozna wplacic ujemnej kwoty; "
-		<< "Wplata anulowana/n";
+	if (amt<0)
+	{
+		cout << "nie mozesz wplacic ujemnej kwoty\n";
+	}
 	else
-		balance +=amt;
+		balance += amt;
+}
+
+void AcctABC::Withdraw(double amt)
+{
+	balance -= amt;
+}
+
+AcctABC::Formatting AcctABC::SetFormat() const
+{
+	Formatting f;
+	f.flag =
+		cout.setf(ios_base::fixed, ios_base::floatfield);
+	f.pr = cout.precision(2);
+	return f;
+}
+
+void AcctABC::Restore(Formatting &f) const
+{
+	cout.setf(f.flag, ios_base::floatfield);
+	cout.precision(f.pr);
 }
 
 void Brass::Withdraw(double amt)
 {
-	//formatowanie na ###.##
-	format initialState = setFormat();
-	precis prec = cout.precision(2);
 	if (amt < 0)
-		cout << "Nie mozna wyplacic ujemnej kwoty; "
-		<< "Wyplata anulowana\n";
-	else if (amt <= balance)
-		balance -= amt;
+		cout << "nie mozna wplacic ujemnej kwoty\n";
+	else if (amt <= Balance())
+		AcctABC::Withdraw(amt);
 	else
-		cout << "Zadana suma " << amt
-		<< " zl przekracza dostepne srodki.\n"
-		<< " Wyplata anulowana\n";
-	restore(initialState, prec);
+		cout << "zadana wartosc  " << amt
+		<< " zl przekracza dostepne srodki\n";
+}
+
+void Brass::ViewAcct() const
+{
+	Formatting f = SetFormat();
+	cout << "wlasciciel " << FullName() << endl;
+	cout << " nr rachunku " << AcctNum() << endl;
+	cout << " stan konta " << Balance() << endl;
+	Restore(f);
+
 
 }
 
-double Brass::Balance() const
+BrassPlus::BrassPlus(const string &s, long an, double bal,
+	double ml, double r) :AcctABC(s, an, bal)
 {
-	return balance;
-}
-void Brass::ViewAc() const
-{
-	//format na ###.##
-	format initialState = setFormat();
-	precis prec = cout.precision(2);
-	cout << "Klient: " << fullname << endl;
-	cout << "Numer rachunku: " << accNum << endl;
-	cout << "Stan konta: " << balance << " zl" << endl;
-	restore(initialState, prec);
-}
-
-
-BrassPluss::BrassPluss(const string &s, long an, double bal, double ml, double r) : Brass(s, an, ml)
-{
-	maxloan = ml;
-	owesbank = 0.0;
+	maxLoan = ml;
+	owesBank = 0;
 	rate = r;
+
 }
 
-BrassPluss::BrassPluss(const Brass &ba, double ml, double r):Brass(ba)
-{
-	maxloan = ml;
-	owesbank = 0.0;
-	rate = r;
-}
 
-void BrassPluss::ViewAc() const
+void BrassPlus::ViewAcct() const
 {
-	//format na ###.##
-	format initialState = setFormat();
-	precis prec = cout.precision(2);
-
-	Brass::ViewAc();
-	cout << "limit debetu: " << maxloan << " zl" << endl;
-	cout << "kwota zadluzenia: " << owesbank << " zl" << endl;
+	Formatting f = SetFormat();
+	cout << "wlasciciel " << FullName() << endl;
+	cout << "nr rachunku " << AcctNum() << endl;
+	cout << "saldo " << Balance() << endl;
+	cout << "limit debetu " << maxLoan << " zl" << endl;
+	cout << "kwota zadluzenia " << owesBank << endl;
 	cout.precision(3);
-	cout << "stopa oprocentowania: " << 100 * rate << "%\n";
-	restore(initialState, prec);
+	cout << "stopa oprocentowania " << 100 * rate << "%\n";
+	Restore(f);
 }
 
-void BrassPluss::Withdraw(double amt)
+void BrassPlus::Withdraw(double amt)
 {
-	//format na ###.##
-	format initialState = setFormat();
-	precis prec = cout.precision(2);
+	Formatting f = SetFormat();
 
 	double bal = Balance();
 	if (amt <= bal)
-		Brass::Withdraw(amt);
-	else if (amt <= bal + maxloan - owesbank)
+		AcctABC::Withdraw(amt);
+	else if (amt <= bal + maxLoan - owesBank)
 	{
 		double advance = amt - bal;
-		owesbank += advance * (1.0*rate);
-		cout << "zadluzenie faktyczne: " << advance << " zl" << endl;
-		cout << "odsetki: " << advance * rate << " zl" << endl;
-		Brass::Withdraw(amt);
+		owesBank += advance * (1.0 + rate);
+		cout << "zadluzenie faktyczne " << advance << " zl\n";
+		cout << "odsetki " << advance * rate << " zl\n";
+		Deposit(advance);
+		AcctABC::Withdraw(amt);
+
 	}
 	else
-		cout << "przekroczony limit debetu. operacja anulowana\n";
-	restore(initialState, prec);
-}
-
-format setFormat()
-{
-	return cout.setf(std::ios_base::fixed,
-		std::ios_base::floatfield);
-}
-
-void restore(format f, precis p)
-{
-	cout.setf(f, std::ios_base::floatfield);
-	cout.precision(p);
+		cout << "przekroczony limit debetu";
+	Restore(f);
 }
--- Ksiazka/queue.h
+++ Ksiazka/queue.h
@@ -1,38 +1,58 @@
-#ifndef QUEUE_H_
-#define QUEUE_H_
+#ifndef  STOCK00_H_
+#define  STOCK00_H_
+#include <iostream>
 #include <string>
 
-class Brass
+class AcctABC
 {
 private:
-	std::string fullname;
-	long accNum;
+	std::string fullName;
+	long acctNum;
 	double balance;
+protected:
+	struct Formatting
+	{
+		std::ios_base::fmtflags flag;
+		std::streamsize pr;
+	};
+	const std::string &FullName() const { return fullName; }
+	long AcctNum() const { return acctNum; }
+	Formatting SetFormat() const;
+	void Restore(Formatting &f) const;
 public:
-	Brass(const std::string &s = "brak", long an = -1, double bal = 0.0);
+	AcctABC(const std::string &s = "brak", long an = -1, double bal = 0.0);
 	void Deposit(double amt);
-	virtual void Withdraw(double amt);
-	double Balance() const;
-	virtual void ViewAc() const;
-	virtual ~Brass() {}
+	virtual void Withdraw(double amt) = 0;
+	double Balance() const { return balance; }
+	virtual void ViewAcct() const = 0;
+	virtual ~AcctABC() {};
 };
 
-class BrassPluss : public Brass
+class Brass :public AcctABC
+{
+public:
+	Brass(const std::string &s = "brak", long an = -1,
+		double bal = 0) :AcctABC(s, an, bal) {}
+	virtual void Withdraw(double amt);
+	virtual void ViewAcct() const;
+	virtual ~Brass();
+};
+
+
+class BrassPlus : public AcctABC
 {
 private:
-	double maxloan;
+	double maxLoan;
 	double rate;
-	double owesbank;
+	double owesBank;
 public:
-	BrassPluss(const std::string & s = "brak", long an = -1, double bal = 0.0,
-		double ml = 2000, double r = 0.11125);
-	BrassPluss(const Brass & ba, double ml = 2000, double r = 0.11125);
-	virtual void ViewAc() const;
+	BrassPlus(const std::string &s = "brak", long an = -1,
+		double bal = 0, double ml = 500, double r = 0.10);
+	BrassPlus(const Brass &ba, double ml = 500, double r = 0.1);
+	virtual void ViewAcct() const;
 	virtual void Withdraw(double amt);
-	void ResetMax(double m) {maxloan = m;}
+		void ResetMax(double m) { maxLoan = m; }
 	void ResetRate(double r) { rate = r; }
-	void ResetOwes() { owesbank = 0; }
+	void ResetOwes() { owesBank = 0; }
 };
-
-
-#endif // !QUEUE_H_
+#endif 
