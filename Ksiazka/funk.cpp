4b1300f0fe23b74027e0b21f9116873df5810d33
Jakub Langowski <kusciana@gmail.com>
2018-09-03 06:00:44 +0200

    dziedziczenie

--- Ksiazka/funk.cpp
+++ Ksiazka/funk.cpp
@@ -9,3 +9,11 @@ void Tabletenisplayer::Name() const
 	std::cout << lastname << ", " << firstname;
 }
 
+Ratedplayer::Ratedplayer(unsigned int r, const string &fn, const string &ln, bool ht) : Tabletenisplayer(fn, ln, ht)
+{
+	rating = r;
+}
+
+Ratedplayer::Ratedplayer(unsigned int r, const Tabletenisplayer &tp): Tabletenisplayer(tp), rating(r)
+{}
+
--- Ksiazka/ksiazka.h
+++ Ksiazka/ksiazka.h
@@ -18,5 +18,15 @@ private:
 	bool hastable;
 };
 
-
+class Ratedplayer : public Tabletenisplayer
+{
+private:
+	unsigned int rating;
+public:
+	Ratedplayer(unsigned int r = 0, const string &fn = "brak",
+		const string &ln = "brak", bool ht = false);
+	Ratedplayer(unsigned int r, const Tabletenisplayer &tp);
+	unsigned int Rating() const { return rating; }
+	void Resetrating(unsigned int r) { rating = r; }
+};
 #endif 
